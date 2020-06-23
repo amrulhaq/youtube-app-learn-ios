6c2fcf851529d56a1fc605dbc1217eea0e98fe0c
Ahmad Qosam Amrul Haq <ahmadqosam.amrulhaq@photoninfotech.net>
2020-06-23 17:10:21 +0700

    Initial Commit

---
+++ youtube-app-learnTests/youtube_app_learnTests.swift
@@ -0,0 +1,34 @@
+//
+//  youtube_app_learnTests.swift
+//  youtube-app-learnTests
+//
+//  Created by Ahmad Qosam Amrul Haq on 6/16/20.
+//  Copyright © 2020 Ahmad Qosam Amrul Haq. All rights reserved.
+//
+
+import XCTest
+@testable import youtube_app_learn
+
+class youtube_app_learnTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
